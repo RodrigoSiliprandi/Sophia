8cc6c1362e63e0483e87980648038fdc88a9481f
RodrigoSiliprandi <rodrigosiliprandi.si@gmail.com>
2021-03-25 22:51:39 -0300

    Adicionando Reconhecedor

---
+++ main.py
@@ -0,0 +1,13 @@
+#Our main file.
+ 
+import speech_recognition as sr
+#Cria um reconhecedor
+r = sr.Recognizer()
+
+#Abrir o microfone para captura
+with sr.Microphone() as source:
+    while True:
+        audio = r.listen(source)
+    
+        print(r.recognize_google(audio, language='pt-PT'))
+
